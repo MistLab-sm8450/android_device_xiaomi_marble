d130eaa7af9bafd4570c93c356b428411ac3414d
SebaUbuntu <seba@sebaubuntu.dev>
2024-04-10 04:21:20 +0900

    marble: Import libinit from old tree
    Co-Authored-By: Ramii Ahmed <ramy@ahmedramy.com>
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Co-Authored-By: Arian <arian.kulmer@web.de>
    Co-Authored-By: Mohammed Althaf Thayyil <me@althafvly.com>
    Co-Authored-By: YuKongA <70465933+YuKongA@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-marble/device.mk)
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_marble)
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.marble.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.marble.rc
