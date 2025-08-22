ed4b40b25293f1326080f4b0ffd9b308a0822087
dhanush281 <dhanushvozh345@gmail.com>
2025-08-22 23:32:52 +0530

    marble:Added Vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,31 @@
+echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
+
+echo 'Cloning Device Common Tree'
+	git clone https://github.com/dhanush281/android_device_xiaomi_sm8450-common.git device/xiaomi/sm8450-common
+
+echo 'Cloning MemeCamera Tree'
+    git clone https://github.com/dhanush281/android_device_xiaomi_miuicamera-marble.git -b lineage-23.0 device/xiaomi/miuicamera-marble
+
+    git clone https://gitlab.com/xiaomi-sm8450-marble/android_vendor_xiaomi_miuicamera-marble.git -b lineage-23.0 vendor/xiaomi/miuicamera-marble
+
+echo 'Cloning Hardware Dolby Tree'
+    git clone https://github.com/dhanush281/android_hardware_dolby.git -b lineage-23.0 hardware/dolby
+
+echo 'Cloning Hardware Xiaomi Tree'
+	git clone https://github.com/dhanush281/android_hardware_xiaomi.git hardware/xiaomi
+
+echo 'Cloning Kernel Tree'
+    git clone https://github.com/dhanush281/android_kernel_xiaomi_sm8450.git kernel/xiaomi/sm8450
+    git clone https://github.com/dhanush281/android_kernel_xiaomi_sm8450-devicetrees.git kernel/xiaomi/sm8450-devicetrees
+    git clone https://github.com/dhanush281/android_kernel_xiaomi_sm8450-modules.git kernel/xiaomi/sm8450-modules
+
+echo 'Cloning Vendor Tree'
+    git clone https://github.com/dhanush281/proprietary_vendor_xiaomi_marble.git vendor/xiaomi/marble
+
+echo 'Cloning Vendor Common Tree'
+    git clone https://github.com/dhanush281/proprietary_vendor_xiaomi_sm8450-common.git vendor/xiaomi/sm8450-common
+
+echo 'cloning wfd tree'
+    git clone https://github.com/dhanush281/android_device_qcom_wfd.git device/qcom/wfd
+
+    git clone https://github.com/dhanush281/android_vendor_qcom_wfd.git vendor/qcom/wfd
