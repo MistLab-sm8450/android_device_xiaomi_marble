d8bd79bac3de13046572372cd9d50b9530f8e226
dhanush281 <dhanushvozh345@gmail.com>
2025-10-04 11:17:05 +0530

    marble: It's RisingOS

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -10,6 +10,18 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RisingOS stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := false
+WITH_GMS := true
+TARGET_INCLUDE_ACCORD := false
+
+# Quick Switch
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
 PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
@@ -18,3 +30,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := POCO/marble_global/marble:15/AQ3A.241006.001/OS2.0.5.0.VMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer stuff
+RISING_MAINTAINER=Dhanush
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon® 7+ Gen 2" \
+    RisingMaintainer="Dhanush"
