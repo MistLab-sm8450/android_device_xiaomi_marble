f0a02e59f9a3ff633a2e66a5c6ffbda7a18eb786
chaitanya <chaitanyaarora3@gmail.com>
2025-10-11 21:55:34 +0530

    marble: Update blobs from OS2.0.209.0.VMRMIXM

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -33,8 +33,11 @@ PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := POCO/marble_global/marble:15/AQ3A.241006.001/OS2.0.5.0.VMRMIXM:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="marble_global-user 15 AQ3A.241006.001 OS2.0.209.0.VMRMIXM release-keys" \
+    BuildFingerprint=POCO/marble_global/marble:15/AQ3A.241006.001/OS2.0.209.0.VMRMIXM:user/release-keys \
+    DeviceProduct=marble \
+    SystemName=marble_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
